86a17de7fd3697493805b2d4c7f3b5e9baac139e
Hugo Folloni <hugofollogua07@gmail.com>
2021-07-23 20:08:30 -0300

    Delete hugo.js

--- commands/hugo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'criador',
-	description: 'Bot creator!',
-	execute(message) {
-		message.channel.send("Hugo Folloni <@274276597903917076> é o criador deste bot e atual membro desse servidor (eu acho)!\nVocê pode encontrá-lo nesse github: https://github.com/hugofolloni");
-	},
-};
